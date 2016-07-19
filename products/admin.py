9f2af209cd0870d6017963e2295d9fd8f6f813ff
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-19 15:54:05 +0900

    admin 파일 설정 추가

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Product, Category
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','updated_at', 'created_at']
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Category)
