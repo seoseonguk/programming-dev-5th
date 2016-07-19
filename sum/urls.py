20d2bb001f0a634c9992f6c934e388a9aaeb288d
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-19 16:02:30 +0900

    SUM APP 추가

---
+++ sum/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from sum import views
+
+
+urlpatterns = [
+
+	url(r'(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)', views.mysum),
+	url(r'(?P<x>\d+)/(?P<y>\d+)/', views.mysum),
+	url(r'(?P<x>\d+)/', views.mysum),
+		
+]
