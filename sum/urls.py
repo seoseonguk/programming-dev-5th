c6014efaeacd31a62e4a5747a1cce335ec10424a
Seonguk, Seo <su.seo@bakecomapany.net>
2016-07-19 16:26:26 +0900

    url 코드 개선

--- sum/urls.py
+++ sum/urls.py
@@ -3,9 +3,9 @@ from sum import views
 
 
 urlpatterns = [
-
-	url(r'(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)', views.mysum),
-	url(r'(?P<x>\d+)/(?P<y>\d+)/', views.mysum),
-	url(r'(?P<x>\d+)/', views.mysum),
+	url(r'(?P<x>[\d/]+)/$', views.mysum2),
+	url(r'(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)/$', views.mysum),
+	url(r'(?P<x>\d+)/(?P<y>\d+)/$', views.mysum),
+	url(r'(?P<x>\d+)/$', views.mysum),
 		
 ]
--- sum/views.py
+++ sum/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def mysum(request, x, y=0, z=0):
 	return HttpResponse(int(x) + int(y) + int(z))
+
+
+def mysum2(request, x):
+	result = sum( int(i) for i in x.split('/'))
+	return HttpResponse(result)
+
+
+
